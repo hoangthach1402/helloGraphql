848df17d019f91b01c8959a36543bc8295baeb56
Vu Hoang Thach <hoangthach1402@Vus-MacBook-Air.local>
2022-04-01 22:59:47 -0700

    .

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const { ApolloServer, gql } = require('apollo-server');
-
+const mongoose = require('mongoose'); 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
+mongoose.connect(
+    "mongodb+srv://hoangthach1402:hoangthach123@cluster0.mmtet.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      // useCreateIndex: true,
+    },
+    (err) => {
+      if (!err) {
+        console.log("MongoDB Connection Succeeded.");
+      } else {
+        console.log("Error in DB connection: " + err);
+      }
+    }
+  );
+
 const typeDefs = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
-    title: String
-    author: String
-  }
+    id:ID 
+    name: String
+    genre: String
+   
+}
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
@@ -19,20 +36,22 @@ const typeDefs = gql`
     books: [Book]
   }
 `;
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
+// const books = [
+//     {
+//       title: 'The Awakening',
+//       author: 'Kate Chopin',
+//     },
+//     {
+//       title: 'City of Glass',
+//       author: 'Paul Auster',
+//     },
+//   ];
 
 const resolvers = {
   Query: {
-    books: () => books,
+    books: async () => {
+        return await Book.find();
+      },
   },
 };
 
