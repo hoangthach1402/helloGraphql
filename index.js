0ef881477ee7c04134e150beaa99178f2ccc5e9e
Vu Hoang Thach <hoangthach1402@Vus-MacBook-Air.local>
2022-04-02 00:02:38 -0700

    .

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { ApolloServer, gql } = require('apollo-server');
+// const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose'); 
 const Book = require('./Model/Book')
+const express = require('express')
+const { ApolloServer } = require('apollo-server-express')
+ 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
